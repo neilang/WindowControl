1fdfaedb7979b4c8d1f88a4433d3c612316b2a0e
Neil Ang <neilang@gmail.com>
2011-04-30 22:02:56 +1000

    Added safari extension

---
+++ WindowControl.safariextension/main.js
@@ -0,0 +1,4 @@
+var script  = document.createElement('script');
+script.type = 'text/javascript';
+script.src  = 'https://github.com/neilang/WindowControl/raw/master/windowcontrol.js';
+document.body.appendChild(script);
