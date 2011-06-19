11104151e67a94d67add485f6a0c23248a174c8b
Neil Ang <neilang@gmail.com>
2011-06-19 19:25:23 +1000

    Added the ability to toggle settings on and off.
    Currently the settings aren't applied onload, but I'll fix that in the next commit

--- WindowControl.safariextension/main.js
+++ WindowControl.safariextension/main.js
@@ -1,4 +1,30 @@
-var script  = document.createElement('script');
-script.type = 'text/javascript';
-script.src  = safari.extension.baseURI + 'windowcontrol.js';
-document.body.appendChild(script);
+function injectJavaScript(text) {
+  var id = "windowcontrol-" + (new Date()).getTime();
+  text += "document.body.removeChild(document.getElementById('" + id + "'));";
+  
+  var data    = document.createTextNode(text);
+  var script  = document.createElement('script');
+  script.type = 'text/javascript';
+  script.id   = id;
+  script.appendChild(data);
+  document.body.appendChild(script);
+}
+
+function injectBaseFunctions() {
+  var script  = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src  = safari.extension.baseURI + 'windowcontrol.js';
+  document.body.appendChild(script);
+}
+
+function handleMessage(event) {
+  if(event.name === "injectThis"){
+    injectJavaScript(event.message);
+  }
+}
+
+// Setup page
+injectBaseFunctions();
+
+// Listen for messages from global.html
+safari.self.addEventListener("message", handleMessage, false);
--- WindowControl.safariextension/windowcontrol.js
+++ WindowControl.safariextension/windowcontrol.js
@@ -1,24 +1,44 @@
+var __windowcontrol_disallowResize = true;
+var __windowcontrol_disallowMove   = true;
+
 var __window_open = window.open;
 window.open = function(url, name, properties) {
+  // TODO: remove disallowed properties on window open
   return __override_windowcontrols(__window_open(url, name));
 };
 
-function __override_windowcontrols (win) {
+function __override_windowcontrols(win) {
+  var __window_resizeTo = win.resizeTo;
   win.resizeTo = function(w, h) {
-   return null;
+    if(__windowcontrol_disallowResize){
+      return null;
+    }
+    return __window_resizeTo(w, h);
   };
   
+  var __window_resizeBy = win.resizeBy;
   win.resizeBy = function(w, h) {
-    return null;
+    if(__windowcontrol_disallowResize){
+      return null;
+    }
+    return __window_resizeBy(w, h);
   };
   
+  var __window_moveTo = win.moveTo;
   win.moveTo = function(x, y){
-    return null;
-  }
+    if(__windowcontrol_disallowMove){
+      return null;
+    }
+    return __window_moveTo(x, y);
+  };
   
+  var __window_moveBy = win.moveBy;
   win.moveBy = function(x, y){
-    return null;
-  }
+    if(__windowcontrol_disallowMove){
+      return null;
+    }
+    return __window_moveBy(x, y);
+  };
   
   return win;
 }
